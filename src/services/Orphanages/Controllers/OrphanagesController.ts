92103db2d221fef653643d8f6be94c2497334ce4
V1n1c1us <v.f.diehl@gmail.com>
2020-10-15 23:58:31 -0300

    Fields validation using Yup and added cors

--- src/services/Orphanages/Controllers/OrphanagesController.ts
+++ src/services/Orphanages/Controllers/OrphanagesController.ts
@@ -1,8 +1,9 @@
 import { Request, Response} from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
 import Orphanages from '../Entities/Orphanages';
 import orphanagesView from '../Views/orphanages_view';
-
 export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanages);
@@ -16,7 +17,7 @@ export default {
 
   async create(request: Request, response: Response) {
     const {
-      nome,
+      name,
       latitude,
       longitude,
       about,
@@ -30,9 +31,10 @@ export default {
     const requestImages = request.files as Express.Multer.File[];
     const images = requestImages.map(image => {
       return { path: image.filename };
-    })
-    const orphanages = orphanagesRepository.create({
-      nome,
+    });
+
+    const data = {
+      name,
       latitude,
       longitude,
       about,
@@ -40,7 +42,27 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    };
+   
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanages = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanages);
 
